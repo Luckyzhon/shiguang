9fec3a5b8e5e02a5243388310896f13d83968963
DavidKennen <799348314@qq.com>
2019-08-16 16:50:11 +0800

    tabbar iconfont commit

--- src/pages/common/TabBar/Tabbar.js
+++ src/pages/common/TabBar/Tabbar.js
@@ -5,10 +5,10 @@ import "./style.scss"
 class Tabbar extends Component {
     state = {
         footerList:[
-            {id:1,path:"/home",name:"首页"},
-            {id:2,path:"/search/1",name:"发现"},
-            {id:3,path:"/article",name:"文章"},
-            {id:4,path:"/mine",name:"我的"}
+            {id:1,path:"/home",name:"首页",icon:"icon-home "},
+            {id:2,path:"/search/1",name:"发现",icon:"icon-search"},
+            {id:3,path:"/article",name:"文章",icon:"icon-article"},
+            {id:4,path:"/mine",name:"我的",icon:"icon-mine"}
         ]
     }
     render() {
@@ -18,7 +18,10 @@ class Tabbar extends Component {
                     
                     <ul className="tabbar">
                         {this.state.footerList.map(item=>(
-                             <li key={item.id}><NavLink to={item.path}>{item.name}</NavLink></li>
+                             <li key={item.id}><NavLink to={item.path}>
+                      <i className={`iconfont ${item.icon}`}></i>
+                          <span > {item.name}    </span>
+                             </NavLink></li>
                         ))}
                        
                     </ul>
